6046c123ea53e4ca8743849f6292cf4decb3e1f7
Surajit Nandy <surajit03011999@gmail.com>
2024-09-02 03:00:29 +0530

    Changes done

--- FileDownloadStreamProcessing/Controllers/FileDownloadController.cs
+++ FileDownloadStreamProcessing/Controllers/FileDownloadController.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace FileDownloadAPI.Controllers
+namespace FileDownloadStreamProcessing.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
---
+++ FileDownloadStreamProcessing/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileDownloadStreamProcessing.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Greet()
+        {
+            return new JsonResult(new {
+                message = "Hello, World!"
+            });
+        }
+    }
+}
